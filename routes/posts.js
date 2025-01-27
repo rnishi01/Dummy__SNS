944a4791bf5735888b29813fc2126082b3919457
rnishi01 <nryushin5403@gmail.com>
2025-01-27 14:15:10 +0900

    投稿機能、いいね機能の追加

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,108 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User.js");
 
-router.get("/", (req,res) =>{
-    res.send("poata router");
+//投稿を作成する
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost =  await newPost.save();
+        return res.status(200).json(savedPost);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+//投稿を更新する
+router.put("/:id" , async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({
+                $set: req.body,
+            });
+            return res.status(200).json("投稿に成功しました！");
+        }else {
+            return res.status(403).json("自分自身以外の投稿は編集できません");
+        }
+    }catch(err) {
+        return res.status(403).json(err);
+    }
+}
+);
+
+//投稿を削除する
+router.delete("/:id" , async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne({
+                $set: req.body,
+            });
+            return res.status(200).json("投稿削除に成功しました！");
+        }else {
+            return res.status(403).json("自分自身以外の投稿は削除できません");
+        }
+    }catch(err) {
+        return res.status(403).json(err);
+    }
+}
+);
+
+//特定の投稿を取得
+router.get("/:id" , async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        return res.status(200).json(post);
+    }catch(err) {
+        return res.status(403).json(err);
+    }
+}
+);
+
+//いいね機能
+router.put("/:id/like", async (req, res) => {
+        try {
+            const post = await Post.findById(req.params.id);
+            //まだいいねを押していなかったらいいねできる
+            if(!post.likes.includes(req.body.userId)) {
+                await post.updateOne({
+                    $push: {
+                     likes: req.body.userId,
+                    },
+                });
+                return res.status(200).json("いいねに成功しました");
+            } else {
+                //いいねを取り消す
+                await post.updateOne({
+                    $pull: {
+                        likes: req.body.userId,
+                    },
+                });
+                return res
+                 .status(403)
+                 .json("いいねを解除しました。");
+            }
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+});
+
+//タイムラインを取得
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id});
+        //フォローしている人の投稿をすべて取得
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId});
+            })
+        );
+        return res.status(200).json(userPosts.concat(...friendPosts));
+    } catch (error) {
+        return res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -65,7 +65,7 @@ router.put("/:id/follow", async (req, res) => {
                 });
                 await currentUser.updateOne({
                     $push: {
-                        followings: req.body.id,
+                        followings: req.params.id,
                     },
                 });
                 return res.status(200).json("フォローに成功しました");
@@ -81,6 +81,42 @@ router.put("/:id/follow", async (req, res) => {
         return res.status(500).json("自分のアカウントはフォローできません。");
     }
 });
+
+
+//ユーザーのフォロー解除
+router.put("/:id/unfollow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            //自分のフォロワーにいる場合解除できる
+            if(user.followers.includes(req.body.userId)){
+                await user.updateOne({
+                    $pull: {
+                     followers: req.body.userId,
+                    },
+                });
+                await currentUser.updateOne({
+                    $pull: {
+                        followings: req.params.id,
+                    },
+                });
+                return res.status(200).json("フォロー解除しました");
+            } else {
+                return res
+                 .status(403)
+                 .json("このユーザーをフォロー解除できません。");
+            }
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    }else {
+        return res.status(500).json("自分のアカウントはフォロー解除できません。");
+    }
+});
+
+
+
 
 
 
