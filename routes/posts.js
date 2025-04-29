e863be909c234ace7d8ac4153230a32185a42051
rnishi01 <nryushin5403@gmail.com>
2025-04-29 22:27:08 +0900

    タイムライン取得APIの修正とユーザー情報取得APIのクエリ対応

--- routes/posts.js
+++ routes/posts.js
@@ -89,9 +89,9 @@ router.put("/:id/like", async (req, res) => {
 });
 
 //タイムラインを取得
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
     try {
-        const currentUser = await User.findById(req.body.userId);
+        const currentUser = await User.findById(req.params.userId);
         const userPosts = await Post.find({ userId: currentUser._id});
         //フォローしている人の投稿をすべて取得
         const friendPosts = await Promise.all(
@@ -100,6 +100,17 @@ router.get("/timeline/all", async (req, res) => {
             })
         );
         return res.status(200).json(userPosts.concat(...friendPosts));
+    } catch (error) {
+        return res.status(500).json(err);
+    }
+});
+
+//プロフィール専用のタイムラインを取得
+router.get("/profile/:username", async (req, res) => {
+    try {
+        const currentUser = await User.findOne({username:req.params.username});
+        const posts = await Post.find({ userId: currentUser._id});
+        return res.status(200).json(posts);
     } catch (error) {
         return res.status(500).json(err);
     }
